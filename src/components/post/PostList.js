e6650c3dc896533b9f921a224b21b2d137659737
Aaron Bassett <me@aaronbassett.com>
2020-05-06 12:58:14 -0400

    Fix bug with re-render of PostList

--- src/components/post/PostList.js
+++ src/components/post/PostList.js
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import _ from 'lodash'
 import { useGlobalState } from '../../state'
 import Post from './Post'
 
 function PostList() {
     const [state, dispatch] = useGlobalState()
-    const [posts, setPosts] = useState()
+    const [loading, setLoading] = useState(true)
+    const [posts, setPosts] = useState([])
 
     useEffect(() => {
         async function fetchPosts() {
             const response = await axios.get(`${state.server_url}/posts`)
             const data = await response.data
-            if (!_.isEqual(state.posts, data)) {
-                dispatch({
-                    posts: data
-                })
-            }
+            dispatch({
+                posts: data
+            })
+            setLoading(false)
         }
         fetchPosts()
-    }, [state.server_url])
+    }, [state.server_url, dispatch])
+
 
     useEffect(() => {
-        const newPosts = state.posts.map((post) =>
-            <Post post={post} key={post._id} />
-        )
-        setPosts(newPosts)
-    }, [state.posts])
+        if (!loading) {
+            const newPosts = state.posts.map((post) =>
+                <Post post={post} key={post._id} />
+            )
+            setPosts(newPosts)
+        }
+    }, [loading, state.posts])
+
 
     return (
         <>
