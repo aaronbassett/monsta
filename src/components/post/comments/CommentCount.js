06439af2ae1c0eaeb69518d3e4070daf8deeab9b
Aaron Bassett <me@aaronbassett.com>
2020-05-07 13:52:31 -0400

    Fix comment count to account for comment replies

--- src/components/post/comments/CommentCount.js
+++ src/components/post/comments/CommentCount.js
@@ -4,9 +4,13 @@ import { MessageTwoTone } from '@ant-design/icons';
 import CountUp from 'react-countup'
 import Pluralize from 'react-pluralize'
 import { HashLink as Link } from 'react-router-hash-link'
+import _ from 'lodash'
 
 function CommentCount(props) {
-    const count = props.comments.length
+    const count = _.reduce(props.comments, (runningCount, comment) => {
+        return ("comments" in comment) ? runningCount + comment.comments.length + 1 : runningCount + 1
+    }, 0)
+
     return (
         <Link to={`/p/${props.post._id}#comments`}>
             <Space>
